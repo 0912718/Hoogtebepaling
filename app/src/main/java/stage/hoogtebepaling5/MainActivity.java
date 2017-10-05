9a7d90fb8741efd30628afcbd3a81b647b16e686
Edgar Buyten <edgarbuyten@gmail.com>
2017-10-05 10:11:04 +0200

    App checkt op wifi en laat vervolgens plattegrond zien

--- app/src/main/java/stage/hoogtebepaling5/MainActivity.java
+++ app/src/main/java/stage/hoogtebepaling5/MainActivity.java
@@ -1,17 +1,16 @@
 package stage.hoogtebepaling5;
 
 import android.app.AlertDialog;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public DatabaseOpenHelper mDBHelper;
-    public SQLiteDatabase mDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,22 +21,23 @@ public class MainActivity extends AppCompatActivity {
         mDBHelper = new DatabaseOpenHelper(this);
         if (wifi.isWifiEnabled()){
               //TODO  Load Map
-            System.out.println("lol1");
-            System.out.println("lol2");
-             ListView listview = (ListView) findViewById(R.id.listView);
-            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
-            databaseAccess.open();
-            List<String> lijst = databaseAccess.getData();
-            databaseAccess.close();
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lijst);
-            listview.setAdapter(adapter);
+            ImageView image = (ImageView) findViewById((R.id.plattegrond));
+            image.setImageResource(R.drawable.beganegrond);
+//             ListView listview = (ListView) findViewById(R.id.listView);
+//            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+            //Database access!!!!!
+//            databaseAccess.open();
+//            List<String> lijst = databaseAccess.getData();
+//            databaseAccess.close();
+//            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lijst);
+//            listview.setAdapter(adapter);
         }
         else{
             AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
             dialog.setCancelable(false);
             dialog.setTitle("The app does not work with Wi-Fi turned off");
             dialog.setMessage("Do you want to turn on Wi-Fi?" );
-            dialog.setPositiveButton("Turn on", (dialog1, id) -> {
+            dialog.setPositiveButton("Turn me on", (dialog1, id) -> {
                 wifi.setWifiEnabled(true);
             })
                     .setNegativeButton("Cancel ", (dialog12, which) -> {
